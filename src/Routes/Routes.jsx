9eea6e2b767f606c77709fc105b0d1a81e745391
mehedi hasan <mhshohan04@gmail.com>
2025-10-05 15:53:40 +0600

    added navbar and footer section

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { createBrowserRouter } from "react-router";
+import Root from '../pages/Roots/Root';
+import Error from '../pages/ErrorPage/Error';
+import Home from '../pages/Home/Home';
 
 
 
 export  const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        path: '/',
+        element: <Home></Home>,
+      }
+    ]
   },
 ])
