c7b82f1fb362af8b5337b9b5f7e08eed69851412
mehedi hasan <mhshohan04@gmail.com>
2025-10-05 10:12:15 +0600

    project setup with vite + tailwindcss v4 + daisyui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className='text-red-400'>boighor</h2>
+      <button className='btn btn-accent'>Boighor</button>
+    </>
+  )
+}
+
+export default App
